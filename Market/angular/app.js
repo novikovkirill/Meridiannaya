d7d442bc922860575c4615f10549286c15711b82
novikovkirill <newuuchen@gmail.com>
2015-08-19 16:05:19 +0300

    Форма для добавления товара

--- Market/angular/app.js
+++ Market/angular/app.js
@@ -13,13 +13,14 @@
 	      }).
 	      when('/products/addProduct', {
 	        templateUrl: 'Market/angular/creatingFormTemplate.html',
+	        controller: 'formCtrl'
 	      }).
 	      otherwise({
 	        redirectTo: '/products'
 	      });
 	}]);
 
-	prodRep.create({name: "Император Мэйдзи и его Япония", price: 500, amount: 50, shopId: 0});
+	prodRep.create({name: "Boots", price: 500, amount: 50, shopId: 0});
 	prodRep.create({name: "T-Shirt", price: 100, amount: 50, shopId: 0});
 	prodRep.create({name: "Cool T­Shirt", price: 200, amount: 50, shopId: 0});
 	prodRep.create({name: "T-Shirt", price: 150, amount: 10, shopId: 0});
--- Market/angular/controller.js
+++ Market/angular/controller.js
@@ -2,8 +2,22 @@
 	
 	"use strict";
 
-	var ctrl = angular.module('marketAppControllers', []).controller("productTableCtrl", function($scope){
+	var ctrl = angular.module('marketAppControllers', [])
+	.controller("productTableCtrl", function($scope){
 					$scope.prodTable = prodRep.getAll();
+	})
+	.controller("formCtrl", function($scope){
+		$scope.addProduct = function(){
+			var prodName = $scope.name;
+			var prodPrice = $scope.price;
+			var prodAmount = $scope.amount;
+			if (prodName === undefined || prodPrice === undefined || prodAmount === undefined)
+				alert("Ошибка! Все поля должны быть заполнены.")
+			else if (prodName.length < 3)
+				alert("Ошибка! Название товара должно состоять минимум из трёх символов.")
+			else 
+				prodRep.create({name: prodName, price: prodPrice, amount: prodAmount, shopId: 0})
+		}
 	})
 
 	return ctrl;
